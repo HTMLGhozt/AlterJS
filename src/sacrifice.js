d08a3a49c1d34014f6a06286d41940130156475a
etisdew <26851121+etisdew@users.noreply.github.com>
2019-01-14 19:58:08 -0800

    Removed comments and bad spelling.
    I will continue to comment on this block of code as it is integrated into main and possibly the monolith class for testing.

--- src/sacrifice.js
+++ src/sacrifice.js
@@ -1,16 +1,3 @@
-/**
- * The zcope of this file will be to decompose a static asset to its respective monolith
- * allowing override to recomposition. We will have an alias to the class we wish to
- * construct and the filename to represent that.
- *
- * If what we have is newer to a hardcoded and dynamicly updated variable, we will
- * seek the content of header, seperating title and style to its blocks.
- * Seeking all body content we will split scripts to their own block.
- *
- * The compilation shall beable to reproduce the source.
- * Capture blocks using the keywords of the tags producing arrays of results we will
- * concatonate through the functions provided in the monolith.
- */
 const fs = require('fs');
 
 function grepField(pathToFile) {
