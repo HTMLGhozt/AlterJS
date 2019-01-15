4c54dca0b53df4bef9d156110fccf06e7d7e8463
etisdew <26851121+etisdew@users.noreply.github.com>
2019-01-14 19:34:54 -0800

    For some reason I cannot push to my own code!
    I'm very much aggravated about this and feeding this file through the web editor.

--- src/sacrifice.js
+++ src/sacrifice.js
@@ -11,12 +11,39 @@
  * Capture blocks using the keywords of the tags producing arrays of results we will
  * concatonate through the functions provided in the monolith.
  */
+const fs = require('fs');
 
-/**
- *
- * @param {*} type header, title, style, body, script
- * @param {*} source
- */
-function grepField(type, source) {
+function grepField(pathToFile) {
+  let extracted = {};
+  let enumerations = [ "title", "style", "head", "script", "body"];
+  function sliceWithRegex(memRef, type) {
+    let openIndex = null;
+    let closeIndex = null;
+    let tagExpOpen  = `<${type}>`;
+    let tagExpClose = `</${type}>`;
+    let sampleSet = memRef.payload.join('')
+    let tagRegexOpen  = new RegExp(tagExpOpen ).exec(sampleSet);
+    let tagRegexClose = new RegExp(tagExpClose).exec(sampleSet);
+    // This does not allow for attributes to be present on the element itself, regex can expand this to the closing character.
+    // The only attr I could see as valid in these instances would be script sources so as that would be contrary to motive, I'll pass.
+    if (tagRegexOpen) openIndex = tagRegexOpen.index;
+    if (tagRegexClose) closeIndex = tagRegexClose.index + type.length + 3;
+    if (typeof openIndex == 'number' && typeof closeIndex == 'number') {
+      let sampleDecomposition = memRef.payload
+        .splice(openIndex, closeIndex - openIndex)
+        .slice(type.length + 2, -(type.length + 3));
 
+      memRef[type] = sampleDecomposition.join('');
+    } else {
+      console.log(`Unable to find a complete entry for ${type}.`);
+    };
+  };
+  const sourceFile = ('' + fs.readFileSync(pathToFile));
+  extracted.payload = [...sourceFile];
+  enumerations.forEach((label) => sliceWithRegex(extracted, label));
+  extracted.payload = sourceFile;
+
+  return extracted;
 };
+
+console.log(grepField(`${__dirname}\\test.html`));
